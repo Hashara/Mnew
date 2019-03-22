c06c120d4384dc577f754eec0394aab32f276864
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-22 23:28:11 +0530

    Update index.php

--- MVC/app/view/requests/index.php
+++ MVC/app/view/requests/index.php
@@ -1,31 +1,66 @@
 
-<?php $this->setSiteTitle("Requests"); ?>
+<?php $this->setSiteTitle("Contacts"); ?>
 <?php $this->start('body'); ?>
-<h1 class ="text-center red"> My Requests </h1>
+<h1 class ="text-center red"> My Contacts </h1>
 <table class="table table-striped table-condensed table-bordered">
 	<thead>
-		<td>Name</td>
-		<th> Service</th>
-		<th> Description</th>
-		<th> Provider</th>
+		<th> Name</th>
+		<th> Email</th>
+		<th> Mobile</th>
+		<th> Home_phone</th>
 		<th></th>
 	</thead>
 	<body>
-		<?php foreach($this->request as $request): ?>
-				<?= dnd($request)?>
+		<?php foreach($this->contacts as $contact): ?>
+
 			<tr>
 				<td>
 					<a 
-					href="<?=PROOT?>requests/details/<?=$request->id?>">
-					<?= $request->displayName(); ?>
+					href="<?=PROOT?>contacts/details/<?=$contact->id?>">
+					<?= $contact->displayName(); ?>
 					</a>
 				</td>
-				<td><?= $request->service ?></td>
-				<td><?= $request->description; ?></td>
-				<td><?= $request->provider; ?></td>
-				<td><a href="<?=PROOT?>requests/edit/<?=$request->id?>" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-pencil"></i> Edit </a>
+				<td><?= $contact->email; ?></td>
+				<td><?= $contact->cell_phone; ?></td>
+				<td><?= $contact->home_phone; ?></td>
+				<td><a href="<?=PROOT?>contacts/edit/<?=$contact->id?>" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-pencil"></i> Edit </a>
 
-				<a href="<?=PROOT?>requests/delete/<?=$request->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure to Delete <?=$request->displayName()?>')){return false;}"><i class="glyphicon glyphicon-remove"></i> Delete </a></td>
+				<a href="<?=PROOT?>contacts/delete/<?=$contact->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure to Delete <?=$contact->displayName()?>')){return false;}"><i class="glyphicon glyphicon-remove"></i> Delete </a></td>
+			</tr>
+
+		<?php endforeach; ?>
+	</body>
+</table>
+<?php $this->end(); ?>
+
+
+<?php $this->setSiteTitle("Contacts"); ?>
+<?php $this->start('body'); ?>
+<h1 class ="text-center red"> My Contacts </h1>
+<table class="table table-striped table-condensed table-bordered">
+	<thead>
+		<th> Name</th>
+		<th> Email</th>
+		<th> Mobile</th>
+		<th> Home_phone</th>
+		<th></th>
+	</thead>
+	<body>
+		<?php foreach($this->contacts as $contact): ?>
+
+			<tr>
+				<td>
+					<a 
+					href="<?=PROOT?>contacts/details/<?=$contact->id?>">
+					<?= $contact->displayName(); ?>
+					</a>
+				</td>
+				<td><?= $contact->email; ?></td>
+				<td><?= $contact->cell_phone; ?></td>
+				<td><?= $contact->home_phone; ?></td>
+				<td><a href="<?=PROOT?>contacts/edit/<?=$contact->id?>" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-pencil"></i> Edit </a>
+
+				<a href="<?=PROOT?>contacts/delete/<?=$contact->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure to Delete <?=$contact->displayName()?>')){return false;}"><i class="glyphicon glyphicon-remove"></i> Delete </a></td>
 			</tr>
 
 		<?php endforeach; ?>
