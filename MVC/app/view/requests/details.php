2d752ebc39b45f6df8d455dc42b40e64663b561e
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:45:39 +0530

    Update details.php

--- MVC/app/view/requests/details.php
+++ MVC/app/view/requests/details.php
@@ -1,15 +1,15 @@
-<?php $this->setSiteTitle($this->request->displayName()); ?>
+<?php $this->setSiteTitle($this->contact->displayName()); ?>
 <?php $this->start('head'); ?>
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">
 	<a href="<?=PROOT?>requests" class="btn btn-xs btn-default"> Back</a>
-	<h2 class="text-center"><?=$this->request->displayName()?></h2>
+	<h2 class="text-center"><?=$this->contact->displayName()?></h2>
 	<div class="col-md-6">
-		<p><strong><pre> Contact :  </strong><?=$this->request->contact?></pre></p>
-		<p><strong><pre> Bin :  </strong><?=$this->request->bin?></pre></p>
-		<p><strong><pre> Address :  </strong><?=$this->request->address?></pre></p>
+		<p><strong><pre> Contact :  </strong><?=$this->contact->contact?></pre></p>
+		<p><strong><pre> Bin :  </strong><?=$this->contact->bin?></pre></p>
+		<p><strong><pre> Address :  </strong><?=$this->contact->address?></pre></p>
 	</div>
 </div>
 
