38ca28a17454f7ff8b7e1daaaf10c8ea0547bf2a
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:10:00 +0530

    Update add.php

--- MVC/app/view/companys/add.php
+++ MVC/app/view/companys/add.php
@@ -1,9 +1,9 @@
-<?php $this->setSiteTitle("Add Contacts"); ?>
+<?php $this->setSiteTitle("Add Company"); ?>
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">
 	<h2 class="text-center"> Add a Contact </h2>
-	<?php $this->partial('contacts','form'); ?>
+	<?php $this->partial('companys','form'); ?>
 </div>
 
 <?php $this->end(); ?>
