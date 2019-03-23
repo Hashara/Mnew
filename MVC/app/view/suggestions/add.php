bc098abaa98189b15efd0f4d9edc955dfef5daf3
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:45:48 +0530

    Update add.php

--- MVC/app/view/suggestions/add.php
+++ MVC/app/view/suggestions/add.php
@@ -1,9 +1,9 @@
-<?php $this->setSiteTitle("Add Contacts"); ?>
+<?php $this->setSiteTitle("Add suggestions"); ?>
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">
 	<h2 class="text-center"> Add a Contact </h2>
-	<?php $this->partial('contacts','form'); ?>
+	<?php $this->partial('suggestions','form'); ?>
 </div>
 
 <?php $this->end(); ?>
