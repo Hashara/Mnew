b13821115249f5d0657a164a4d07b1610bbdb687
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:09:56 +0530

    Update details.php

--- MVC/app/view/companys/details.php
+++ MVC/app/view/companys/details.php
@@ -4,16 +4,16 @@
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">
-	<a href="<?=PROOT?>contacts" class="btn btn-xs btn-default"> Back</a>
-	<h2 class="text-center"><?=$this->contact->displayaddName()?></h2>
+	<a href="<?=PROOT?>company" class="btn btn-xs btn-default"> Back</a>
+	<h2 class="text-center"><?=$this->contact->name?></h2>
 	<div class="col-md-6">
-		<p><strong><pre> First :  </strong><?=$this->contact->age?></pre></p>
 		<p><strong><pre> Email :  </strong><?=$this->contact->email?></pre></p>
-		<p><strong><pre> Mobile :  </strong><?=$this->contact->cell_phone?></pre></p>
-		<p><strong><pre> Home :  </strong><?=$this->contact->home_phone?></pre></p>
+		<p><strong><pre> Contact Number :  </strong><?=$this->contact->contact?></pre></p>
+		<p><strong><pre> Address :  </strong><?=$this->contact->address?></pre></p>
+		<p><strong><pre> Web Site :  </strong><?=$this->contact->website?></pre></p>
 	</div>
 	<div class="col-md-6">
-		<?=$this->contact->displayAddressLabel()?>
+
 	</div>
 </div>
 
