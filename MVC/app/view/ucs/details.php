b058f92518c9ce63c2e393a58582598dabb26b00
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:46:18 +0530

    Update details.php

--- MVC/app/view/ucs/details.php
+++ MVC/app/view/ucs/details.php
@@ -7,13 +7,9 @@
 	<a href="<?=PROOT?>contacts" class="btn btn-xs btn-default"> Back</a>
 	<h2 class="text-center"><?=$this->contact->displayaddName()?></h2>
 	<div class="col-md-6">
-		<p><strong><pre> First :  </strong><?=$this->contact->age?></pre></p>
-		<p><strong><pre> Email :  </strong><?=$this->contact->email?></pre></p>
-		<p><strong><pre> Mobile :  </strong><?=$this->contact->cell_phone?></pre></p>
-		<p><strong><pre> Home :  </strong><?=$this->contact->home_phone?></pre></p>
-	</div>
-	<div class="col-md-6">
-		<?=$this->contact->displayAddressLabel()?>
+		<p><strong><pre> Name :  </strong><?=$this->contact->name?></pre></p>
+		<p><strong><pre> Contact :  </strong><?=$this->contact->contact?></pre></p>
+
 	</div>
 </div>
 
