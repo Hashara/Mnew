a4900ae5f318786324389a559ed1c1648c9793fb
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:11:11 +0530

    Update form.php

--- MVC/app/view/requests/partials/form.php
+++ MVC/app/view/requests/partials/form.php
@@ -2,10 +2,10 @@
 
 	<div><?=$this->displayErrors ?></div>
 	<!-- HIIIIIIIIIIIIIIIIIIIIIIIIIII -->
-	<?= input_block('text','First name','service',$this->request->service,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<?= input_block('text','Last name','description',$this->request->description,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<?= input_block('text','Address','provider',$this->request->provider,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-
+	<?= input_block('text','Name','name',$this->contact->name,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','Contact','description',$this->contact->contact,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','Address','provider',$this->contact->address,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','Bin','bin',$this->contact->bin,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
 
 	<div class = "col-md-12 text-right">
 		<a href="<?=PROOT?>requests" class="btn btn-default"> Cancel </a>
