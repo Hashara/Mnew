cb0f6909a5953aa9c7f3f3522ed5fe23a881c229
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:09:50 +0530

    Update edit.php

--- MVC/app/view/announcements/edit.php
+++ MVC/app/view/announcements/edit.php
@@ -7,15 +7,8 @@
 <!-- 	<a href="<?=PROOT?>contacts" class="btn btn-xs btn-default"> Back</a>
  -->	
  <h2 class="text-center"><?=$this->contact->displayName()?></h2>
- <?= $this->partial('contacts','form')?>
-	<!-- <div class="col-md-6">
-		<p><strong><pre> Email :  </strong><?=$this->contact->email?></pre></p>
-		<p><strong><pre> Mobile :  </strong><?=$this->contact->cell_phone?></pre></p>
-		<p><strong><pre> Home :  </strong><?=$this->contact->home_phone?></pre></p>
-	</div>
-	<div class="col-md-6">
-		<?=$this->contact->displayAddressLabel()?>
-	</div>
+ <?= $this->partial('announcements','form')?>
+
 </div> -->
 
 <?php $this->end(); ?>
