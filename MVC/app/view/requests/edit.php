66f86dbdd344474d59183c5f11891c1592ee008c
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:11:16 +0530

    Update edit.php

--- MVC/app/view/requests/edit.php
+++ MVC/app/view/requests/edit.php
@@ -1,4 +1,4 @@
-<?php $this->setSiteTitle('Edit Contact'); ?>
+<?php $this->setSiteTitle('Edit Requests'); ?>
 <?php $this->start('head'); ?>
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
@@ -7,14 +7,12 @@
 <!-- 	<a href="<?=PROOT?>contacts" class="btn btn-xs btn-default"> Back</a>
  -->	
  <h2 class="text-center"><?=$this->contact->displayName()?></h2>
- <?= $this->partial('contacts','form')?>
+ <?= $this->partial('requests','form')?>
 	<!-- <div class="col-md-6">
-		<p><strong><pre> Email :  </strong><?=$this->contact->email?></pre></p>
-		<p><strong><pre> Mobile :  </strong><?=$this->contact->cell_phone?></pre></p>
-		<p><strong><pre> Home :  </strong><?=$this->contact->home_phone?></pre></p>
+
 	</div>
 	<div class="col-md-6">
-		<?=$this->contact->displayAddressLabel()?>
+
 	</div>
 </div> -->
 
