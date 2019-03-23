038405568dc93735d0c98095bc16e5307460c0b3
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:45:09 +0530

    Update form.php

--- MVC/app/view/ucs/partials/form.php
+++ MVC/app/view/ucs/partials/form.php
@@ -2,20 +2,11 @@
 
 	<div><?=$this->displayErrors ?></div>
 	<!-- HIIIIIIIIIIIIIIIIIIIIIIIIIII -->
-	<?= input_block('text','First name','fname',$this->contact->fname,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<?= input_block('text','Last name','lname',$this->contact->lname,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<?= input_block('text','Address','address',$this->contact->address,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<?= input_block('text','City','city',$this->contact->city,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<?= input_block('text','Email','email',$this->contact->email,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<?= input_block('text','Service','service',$this->contact->service,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
-	<?= input_block('text','Home Phone','home_phone',$this->contact->home_phone,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
-	<?= input_block('text','Mobile','cell_phone',$this->contact->cell_phone,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
-	<?= input_block('text','Work_phone','work_phone',$this->contact->work_phone,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
-	<?= input_block('text','State','state',$this->contact->state,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
-	<?= input_block('text','Service Type','serviceType',$this->contact->serviceType,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
+	<?= input_block('text','Name','name',$this->contact->name,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+		<?= input_block('text','Contact','contact',$this->contact->contact,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
 
 	<div class = "col-md-12 text-right">
-		<a href="<?=PROOT?>contacts" class="btn btn-default"> Cancel </a>
+		<a href="<?=PROOT?>uc" class="btn btn-default"> Cancel </a>
 
 	<?= submitBlock('Save',['class'=>'btn btn-primary']) ?>
 
