0554c9414eebacce644dfa220501b15d0346d366
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:46:03 +0530

    Update index.php

--- MVC/app/view/suggestions/index.php
+++ MVC/app/view/suggestions/index.php
@@ -1,13 +1,13 @@
 
-<?php $this->setSiteTitle("Contacts"); ?>
+<?php $this->setSiteTitle("suggestions"); ?>
 <?php $this->start('body'); ?>
 <h1 class ="text-center red"> My Contacts </h1>
 <table class="table table-striped table-condensed table-bordered">
 	<thead>
-		<th> Name</th>
-		<th> Email</th>
-		<th> Mobile</th>
-		<th> Home_phone</th>
+		<th> Topic</th>
+		<th> Content</th>
+		<th> Date</th>
+
 		<th></th>
 	</thead>
 	<body>
@@ -16,16 +16,16 @@
 			<tr>
 				<td>
 					<a 
-					href="<?=PROOT?>contacts/details/<?=$contact->id?>">
-					<?= $contact->displayName(); ?>
+					href="<?=PROOT?>suggestions/details/<?=$contact->id?>">
+					<?= $contact->topic; ?>
 					</a>
 				</td>
-				<td><?= $contact->email; ?></td>
-				<td><?= $contact->cell_phone; ?></td>
-				<td><?= $contact->home_phone; ?></td>
-				<td><a href="<?=PROOT?>contacts/edit/<?=$contact->id?>" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-pencil"></i> Edit </a>
+				<td><?= $contact->content; ?></td>
+				<td><?= $contact->date; ?></td>
+				
+				<td><a href="<?=PROOT?>suggestions/edit/<?=$contact->id?>" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-pencil"></i> Edit </a>
 
-				<a href="<?=PROOT?>contacts/delete/<?=$contact->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure to Delete <?=$contact->displayName()?>')){return false;}"><i class="glyphicon glyphicon-remove"></i> Delete </a></td>
+				<a href="<?=PROOT?>suggestions/delete/<?=$contact->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure to Delete <?=$contact->displayName()?>')){return false;}"><i class="glyphicon glyphicon-remove"></i> Delete </a></td>
 			</tr>
 
 		<?php endforeach; ?>
