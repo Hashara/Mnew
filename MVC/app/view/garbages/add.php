82a38b0596f990befc2d30a6caa04dea1fe0f5d2
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:10:14 +0530

    Update add.php

--- MVC/app/view/garbages/add.php
+++ MVC/app/view/garbages/add.php
@@ -1,9 +1,9 @@
-<?php $this->setSiteTitle("Add Contacts"); ?>
+<?php $this->setSiteTitle("Add Garbage Details"); ?>
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">
-	<h2 class="text-center"> Add a Contact </h2>
-	<?php $this->partial('contacts','form'); ?>
+	<h2 class="text-center"> Add Details </h2>
+	<?php $this->partial('garbages','form'); ?>
 </div>
 
 <?php $this->end(); ?>
