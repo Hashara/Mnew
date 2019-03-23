f74a8ef28b8fd14aa1ebc01c91bddb491885b5b2
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:10:29 +0530

    Update index.php

--- MVC/app/view/garbages/index.php
+++ MVC/app/view/garbages/index.php
@@ -1,35 +1,40 @@
 
-<?php $this->setSiteTitle("Contacts"); ?>
+<?php $this->setSiteTitle("Garbages"); ?>
 <?php $this->start('body'); ?>
-<h1 class ="text-center red"> My Contacts </h1>
+<h1 class ="text-center red"> Details </h1>
 <table class="table table-striped table-condensed table-bordered">
 	<thead>
-		<th> Name</th>
-		<th> Email</th>
-		<th> Mobile</th>
-		<th> Home_phone</th>
+		<th> Place</th>
+		<th> Date</th>
+		<th> Amount of Gather gabages</th>
+		<th> Amount of sales gabages</th>
 		<th></th>
 	</thead>
 	<body>
 		<?php foreach($this->contacts as $contact): ?>
-
+			
 			<tr>
 				<td>
 					<a 
-					href="<?=PROOT?>contacts/details/<?=$contact->id?>">
-					<?= $contact->displayName(); ?>
+					href="<?=PROOT?>garbages/details/<?=$contact->id?>">
+					<?= $contact->name; ?>
 					</a>
 				</td>
-				<td><?= $contact->email; ?></td>
-				<td><?= $contact->cell_phone; ?></td>
-				<td><?= $contact->home_phone; ?></td>
-				<td><a href="<?=PROOT?>contacts/edit/<?=$contact->id?>" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-pencil"></i> Edit </a>
+				<td><?= $contact->date; ?></td>
+				<td><?= $contact->plus; ?></td>
+				<td><?= $contact->sale; ?></td>
+				<td><a href="<?=PROOT?>garbages/edit/<?=$contact->id?>" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-pencil"></i> Edit </a>
 
-				<a href="<?=PROOT?>contacts/delete/<?=$contact->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure to Delete <?=$contact->displayName()?>')){return false;}"><i class="glyphicon glyphicon-remove"></i> Delete </a></td>
+				<a href="<?=PROOT?>garbages/delete/<?=$contact->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure to Delete <?=$contact->displayName()?>')){return false;}"><i class="glyphicon glyphicon-remove"></i> Delete </a></td>
 			</tr>
 
 		<?php endforeach; ?>
 	</body>
 </table>
 <?php $this->end(); ?>
+
+
+
+
+?>
 
