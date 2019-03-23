ff2d96bede04c2764daab05eca6d3607e1ef7cb0
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:09:28 +0530

    Update add.php

--- MVC/app/view/announcements/add.php
+++ MVC/app/view/announcements/add.php
@@ -1,9 +1,9 @@
-<?php $this->setSiteTitle("Add Contacts"); ?>
+<?php $this->setSiteTitle("Add Announcement"); ?>
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">
-	<h2 class="text-center"> Add a Contact </h2>
-	<?php $this->partial('contacts','form'); ?>
+	<h2 class="text-center"> Add a Announcement </h2>
+	<?php $this->partial('announcements','form'); ?>
 </div>
 
 <?php $this->end(); ?>
