8c86b7ee9010c5197f89c536d9c9293fc9f2ca64
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:12:35 +0530

    Update GarbagesController.php

--- MVC/app/Controller/GarbagesController.php
+++ MVC/app/Controller/GarbagesController.php
@@ -1,59 +1,81 @@
 <?php
 
-class AccountsController extends Controller
+class GarbagesController extends Controller
 {
-	public function __construct($controller , $action)
-	{
-		parent::__construct($controller , $action);
-		$this->view->setLayout('defaultlay');
-		$this->load_model('Users');
-		//dnd($this->load_model('Contacts'));
-	}
+    public function __construct($controller , $action)
+    {
+        parent::__construct($controller , $action);
+        $this->view->setLayout('defaultlay');
+        $this->load_model('Garbage');
+        //dnd($this->load_model('Contacts'));
+    }
 
 
-  public function indexAction()
-  {
-    // $account = $this->AccountsModel->findByUserId(currentUser()->id);
-    $account=currentUser();
-    // dnd(findByUserId(currentUser()->id,['order'=>'lname, fname']));
-    // dnd($account);
-    $this->view->account=$account;
-    $this->view->render('accounts/index');
-  }
+
+    public function indexAction()
+    {
+        $contacts = $this->GarbageModel->findByUserId(currentUser()->id,['order'=>'name']);
+        // dnd($contacts);fname
+        // dnd($contacts);
+        $this->view->contacts=$contacts;
+        // dnd($contacts);
+        $this->view->render('garbages/index');
+    }
+
+    public function addAction()
+    {   
+        $contact = new Garbage();
+        $validation = new Validate();
+        if($_POST)
+        {
+            $contact->assign($_POST);   //form validation
+            // dnd($contact->assign($_POST));
+        $validation->check($_POST,Garbage::$addValidation);
+
+            
+            // dnd($_POST);
+            
+            if($validation->passed())
+            { 
+            $contact->user_id =currentUser()->id;
+            // dnd(currentUser()->id);
+                // dnd("klk");
+                // dnd($contact->assign($_POST));
+            $contact->deleted=0;
+            // dnd($contact->deleted);
+                // $contact->assign($_POST);
+                // dnd()
+            // dnd($contact->save());
+                $contact->save();
+
+                Router::redirect('garbages');
+            }
+            
+            
+        }
+        // else
+        // {
+
+        //  dnd("jkj");
+        // }
+        $this->view->displayErrors=$validation->displayErrors();
+        $this->view->contact = $contact;
+        $this->view->postAction=PROOT.'garbages'.DS.'add';
+        // dnd($this->view->postAction);
+        $this->view->render('garbages/add');
+    }
+
 
   public function detailsAction($id)
   {
     // dnd($id);
-    $account = currentUser();//cast is a security to check its a number
+    $contact = $this->GarbageModel->findByIdAndUserId((int)$id,currentUser()->id);//cast is a security to check its a number
     // dnd($contact);
-    if(!$account){
-      Router::redirect('accounts');//no contact
+    if(!$contact){
+      Router::redirect('garbages');//no contact
     }
-    $this->view->account = $account;
-    $this->view->render('accounts/details');
-  }
-
-    public function deleteAction($id)
-   {
-    $account = currentUser();
-    $id=$account->id;
-    // dnd($id);
-    // $account->logout();
-    
-    $account = $this->UsersModel->delete($id);
-    // dnd($account->deleted);
-    // echo '5';
-    // dnd($id);
-    // dnd($account->logout());
-// dnd(currentUser());
-    //cast is a security to check its a number
-    // dnd($account);
-    
-    // if($account){
-    //   $account->delete(); 
-      // Session::addMsg('success','Contact has been deleted.');
-   // }
-    Router::redirect('register/logout');
+    $this->view->contact = $contact;
+    $this->view->render('garbages/details');
   }
 
 
@@ -61,87 +83,45 @@ class AccountsController extends Controller
   {
     // dnd($id);
     $validation = new Validate();
-    $account = currentUser();
-    // dnd($contact->fname);
+    $contact = $this->GarbageModel->findByIdAndUserId((int)$id,currentUser()->id);
 
-    if(!$account) Router::redirect('accounts');
+    if(!$contact) Router::redirect('garbages');
 
     if($_POST)
     {
-      $account->assign($_POST); //form validation
-      // dnd($contact->assign($_POST));
-    $validation->check($_POST,[
-                'name'=>[
-                    'display' => 'First Name'
-                    // 'required' => true
-                ],
-                'username' => [
-                    'display' => 'username',
-                    // 'required' => true,
-                    // 'unique' => 'users'
-                    //'valid_email' => true
-                    'min'=> 4
-                    //'max' =>25
-                ],
-                'password' => [
-                    'display' => 'Password',
-                    // 'required' => true,
-                    'min' => 6
-                    //'max' => 100
-                ],
-                'address' => [
-                    'display' => 'Address',
-                    // 'required' => true,
-                    'min' => 6
-                    //'max' => 100
-                ],
-                // 'phoneNumber' => [
-                //     'display' => 'Contact Number 1',
-                //     'required' => true,
-                //     'min' => 10,
-                //     'max' => 10
-                // ],
-                'contact' => [
-                    'display' => 'Contact Number 2',
-                    'min' => 10
-                    //'max' => 100
-                ],
-                'usertype' => [
-                    'display' => 'User type'
-                    // 'required' => true
-                    //'max' => 100
-                ],
-                'confirm' => [
-                    'display' => 'Confirm Password',
-                    // 'required' => true,
-                    'matches' => 'password'
+            $contact->assign($_POST);   //form validation
+            // dnd($contact->assign($_POST));
+        $validation->check($_POST,Garbage::$addValidation);
 
-                ]
-            ]);
+            
+            // dnd($_POST);
+            
+            if($validation->passed())
+            { 
+            // dnd($contact->deleted);
+                // $contact->assign($_POST);
+                // dnd()
+            // dnd($contact->save());
+                $contact->save();
 
-      
-      // dnd($_POST);
-      
-      if($validation->passed())
-      { 
-      // dnd($contact->deleted);
-        // $contact->assign($_POST);
-        // dnd()
-      // dnd($contact->save());
-        $account->save();
-
-        Router::redirect('accounts');
-      }
-    }
+                Router::redirect('garbages');
+            }
+        }
 
     $this->view->displayErrors=$validation->displayErrors();
-    $this->view->account = $account;
-    // $this->view->postAction = PROOT . 'register' . DS . 'edit' . DS . $account->id;
-    $this->view->render('accounts/edit');
+    $this->view->contact = $contact;
+    $this->view->postAction = PROOT . 'garbages' . DS . 'edit' . DS . $contact->id;
+    $this->view->render('garbages/edit');
   }
 
-
+  public function deleteAction($id){
+    $contact = $this->GarbageModel->findByIdAndUserId((int)$id,currentUser()->id);//cast is a security to check its a number
+    // dnd($contact);
+    if($contact){
+      $contact->delete(); 
+      // Session::addMsg('success','Contact has been deleted.');
+    }
+    Router::redirect('garbages');
+  }
 
  }
-
- ?>
