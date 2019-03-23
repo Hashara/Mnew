162ed35b3be0526893f58eaf6ce1c012631dd015
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:45:35 +0530

    Update add.php

--- MVC/app/view/requests/add.php
+++ MVC/app/view/requests/add.php
@@ -1,9 +1,9 @@
-<?php $this->setSiteTitle("Add Contacts"); ?>
+<?php $this->setSiteTitle("Add Requests"); ?>
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">
 	<h2 class="text-center"> Add a Contact </h2>
-	<?php $this->partial('contacts','form'); ?>
+	<?php $this->partial('requests','form'); ?>
 </div>
 
 <?php $this->end(); ?>
