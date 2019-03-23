aebbe73acc31b4d8d24ac9e63bd03863f96b9a93
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:46:08 +0530

    Update add.php

--- MVC/app/view/ucs/add.php
+++ MVC/app/view/ucs/add.php
@@ -1,9 +1,9 @@
-<?php $this->setSiteTitle("Add Contacts"); ?>
+<?php $this->setSiteTitle("Add Uc"); ?>
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">
-	<h2 class="text-center"> Add a Contact </h2>
-	<?php $this->partial('contacts','form'); ?>
+	<h2 class="text-center"> Add a Uc </h2>
+	<?php $this->partial('ucs','form'); ?>
 </div>
 
 <?php $this->end(); ?>
