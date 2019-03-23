1693e49b82065a7f01fdffc2a46ef8cdc775ce09
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-23 07:09:53 +0530

    Update form.php

--- MVC/app/view/announcements/partials/form.php
+++ MVC/app/view/announcements/partials/form.php
@@ -2,17 +2,10 @@
 
     <div><?=$this->displayErrors ?></div>
     <!-- HIIIIIIIIIIIIIIIIIIIIIIIIIII -->
-    <?= input_block('text','First name','fname',$this->contact->fname,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-    <?= input_block('text','Last name','lname',$this->contact->lname,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-    <?= input_block('text','Address','address',$this->contact->address,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-    <?= input_block('text','City','city',$this->contact->city,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-    <?= input_block('text','Email','email',$this->contact->email,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-    <?= input_block('text','Service','service',$this->contact->service,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
-    <?= input_block('text','Home Phone','home_phone',$this->contact->home_phone,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
-    <?= input_block('text','Mobile','cell_phone',$this->contact->cell_phone,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
-    <?= input_block('text','Work_phone','work_phone',$this->contact->work_phone,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
-    <?= input_block('text','State','state',$this->contact->state,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
-    <?= input_block('text','Service Type','serviceType',$this->contact->serviceType,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
+    <?= input_block('text','Topic','topic',$this->contact->topic,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+    <?= input_block('text','Content','content',$this->contact->content,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+    <?= input_block('text','Urban Council','uc',$this->contact->uc,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+
 
     <div class = "col-md-12 text-right">
         <a href="<?=PROOT?>contacts" class="btn btn-default"> Cancel </a>
